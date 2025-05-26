ab8e352adc34dede862fc330e255bd7fe4cfea33
Rodrigo Nava Ramirez <email@rodrigonava.mx>
2025-05-26 13:27:37 +0200

    updated home page font size

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -41,7 +41,7 @@
     <?php endif ?>
     <link rel="icon" type='image/png' href="/assets/logos/favicon.png">
 
-    <?= css('/assets/css/index.css?v=1.3.3') ?>
+    <?= css('/assets/css/index.css?v=1.3.4') ?>
 </head>
 <body>
     <main id='swup' class=' transition-fade main-container <?= str_replace(' ', '-', $page -> template()) ?>' >
